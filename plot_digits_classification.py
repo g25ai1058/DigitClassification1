55bed1d73223dfc4a28228e033ba9d2bbe22ed03
g25ai1058 <g25ai1058@iitj.ac.in>
2026-05-21 11:42:04 +0000

    added a print statement

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -77,3 +77,54 @@ predicted = clf.predict(X_test)
 # Below we visualize the first 4 test samples and show their predicted
 # digit value in the title.
 
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, prediction in zip(axes, X_test, predicted):
+    ax.set_axis_off()
+    image = image.reshape(8, 8)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title(f"Prediction: {prediction}")
+
+###############################################################################
+# :func:`~sklearn.metrics.classification_report` builds a text report showing
+# the main classification metrics.
+
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
+
+###############################################################################
+# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
+# true digit values and the predicted digit values.
+
+disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+disp.figure_.suptitle("Confusion Matrix")
+print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+plt.show()
+
+###############################################################################
+# If the results from evaluating a classifier are stored in the form of a
+# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
+# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
+# as follows:
+
+
+# The ground truth and predicted lists
+y_true = []
+y_pred = []
+cm = disp.confusion_matrix
+
+# For each cell in the confusion matrix, add the corresponding ground truths
+# and predictions to the lists
+for gt in range(len(cm)):
+    for pred in range(len(cm)):
+        y_true += [gt] * cm[gt][pred]
+        y_pred += [pred] * cm[gt][pred]
+
+print(
+    "Classification report rebuilt from confusion matrix:\n"
+    f"{metrics.classification_report(y_true, y_pred)}\n"
+)
+
+print ("Test workflow")
